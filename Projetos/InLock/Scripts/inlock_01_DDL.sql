6c2d2af0f186dd9490e986c5ae50cd4770667b95
Lucas-M3dina <Lucas.m3dina@gmail.com>
2021-09-09 11:11:47 -0300

    ddl

---
+++ Projetos/InLock/Scripts/inlock_01_DDL.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE INLOCK_GAMES_MAN;
+GO
+
+USE INLOCK_GAMES_MAN;
+GO
+
+CREATE TABLE Estudios(
+idEstudio tinyint PRIMARY KEY IDENTITY(1,1),
+nomeEstudio VARCHAR(30) NOT NULL
+);
+GO
+
+CREATE TABLE Jogos(
+idJogo tinyint PRIMARY KEY IDENTITY(1,1),
+nomeJogos VARCHAR(100)NOT NULL ,
+descricaoJogo VARCHAR(1000),
+dataLancamento DATE NOT NULL,
+valor MONEY NOT NULL,
+idEstudio tinyint FOREIGN KEY REFERENCES Estudios(idEstudio)
+);
+GO
+
+CREATE TABLE TipoUsuario(
+idTipoUsuario tinyint PRIMARY KEY IDENTITY(1,1),
+Titulo VARCHAR(50)
+);
+GO
+
+CREATE TABLE Usuario(
+idUsuario tinyint PRIMARY KEY IDENTITY(1,1),
+nomeUsuario VARCHAR(30),
+emailUsuario VARCHAR(100),
+senhaUsuario VARCHAR(30),
+idTipoUsuario tinyint FOREIGN KEY REFERENCES TipoUsuario(idTipoUsuario)
+);
